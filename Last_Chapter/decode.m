de2874e496b4796f22776054c1cd278e319fcb09
aconsilvio <annabel@students.olin.edu>
2015-04-24 11:50:28 -0400

    adding decode

---
+++ Last_Chapter/decode.m
@@ -0,0 +1,10 @@
+function res = decode()
+ message = load('message.csv');
+%  msg_reshaped = reshape(message, [mod(length(message)/8),8]);
+%  message = mat2str(message);
+%  decoded = char(bin2dec(reshape(message,[mod(length(message)/8),8]).').')
+ 
+for i = 1:length(message)
+    
+end 
+end
