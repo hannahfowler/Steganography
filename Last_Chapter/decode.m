5e2b20f68c844ab89bf0666a9e52b3d4682fc38c
aconsilvio <annabel@students.olin.edu>
2015-04-24 12:18:23 -0400

    Adding binary decoding for Hannah

--- Last_Chapter/decode.m
+++ Last_Chapter/decode.m
@@ -3,8 +3,15 @@ function res = decode()
 %  msg_reshaped = reshape(message, [mod(length(message)/8),8]);
 %  message = mat2str(message);
 %  decoded = char(bin2dec(reshape(message,[mod(length(message)/8),8]).').')
- 
-for i = 1:length(message)
-    strings = mat2str(message(i):message(i+
+for i = 0:7:length(message)
+    binary = message(1+i:8+i);
+    binary_array = mat2str(binary);
+    binary_string = binary_array(2:16);
+    for i = 1:length(binary_string)
+        if binary_string(i) == 0 or 1
+            binary_letter = binary_letter + binary(i) 
+        end 
+    end 
+    
 end 
 end
