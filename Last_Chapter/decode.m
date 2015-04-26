902a1d7a1ca27e8693d3d1dcb4c13c0eb9a69a10
Hannah Jo Fowler <hannahjo@hannahs-mbp.olin.edu>
2015-04-26 12:35:06 -0400

    Adding code to decode message

--- Last_Chapter/decode.m
+++ Last_Chapter/decode.m
@@ -1,17 +1,24 @@
 function res = decode()
- message = load('message.csv');
+ %message = load('message.csv');
+ new = [0 1 0 0 1 1 0 1 0 1 1 1 1 0 0 1 0 0 1 0 0 0 0 0 0 1 1 0 1 1 1 0 0 1 1 0 0 0 0 1 0 1 1 0 1 1 0 1 0 1 1 0 0 1 0 1 0 0 1 0 0 0 0 0 0 1 1 0 1 0 0 1 0 1 1 1 0 0 1 1 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 1 0 1 1 0 1 1 1 0 0 1 1 0 1 1 1 0 0 1 1 0 0 0 0 1 0 1 1 0 0 0 1 0 0 1 1 0 0 1 0 1 0 1 1 0 1 1 0 0];
 %  msg_reshaped = reshape(message, [mod(length(message)/8),8]);
 %  message = mat2str(message);
 %  decoded = char(bin2dec(reshape(message,[mod(length(message)/8),8]).').')
-for i = 0:7:length(message)
-    binary = message(1+i:8+i);
+for i = 1:8:length(new)-7
+    binary = new(i:i+7); 
     binary_array = mat2str(binary);
-    binary_string = binary_array(2:16);
+    if binary == [0 0 0 0 0 0 0 0]
+        break 
+    end
+    binary_string = binary_array(2:16); %rids of brackets
+    binary_letter = '';
+    code = '';
     for i = 1:length(binary_string)
-        if binary_string(i) == 0 or 1
-            binary_letter = binary_letter + binary(i) 
-        end 
+        if (binary_string(i)== '1' | binary_string(i)== '0')
+            binary_letter = strcat(binary_letter, binary_string(i));
+            mess = char(bin2dec(binary_letter));
+            code = strcat(code,mess);
     end 
-    
-end 
+    end 
+disp(code)
 end
